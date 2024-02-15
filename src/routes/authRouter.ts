78dc4ddede793aa005b6b41dee7c82842004478d
NicolasCirulli <nicolascirulli2@gmail.com>
2024-02-15 13:39:44 -0300

    Add: routes Auth && users

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,8 +2,11 @@ import express from 'express'
 
 const authRouter = express.Router()
 
-authRouter.get( '/', (_req, res) => {
-    res.send('Auth endpoint')
-})
+const aux = (message:string) => (_req: any, res: any) => {res.send(message)}
+
+authRouter.post( '/login', aux('Login'))
+authRouter.post( '/register', aux('Register'))
+authRouter.post( '/register/google', aux('Register google'))
+
 
 export default authRouter
